a69bfa0a69d4398191dcd2cae8c73949b59dc7fe
Anil Singh <anillibra76@gmail.com>
2017-07-16 00:52:08 +0530

    Removed all functons

--- employee.ts
+++ employee.ts
@@ -3,12 +3,9 @@ interface Iemp {
     _id: number;
     name: string;
 }
-// This is Employee class
-// Another class comments
+
 export class Employee {
     employees: Iemp[];
 
-    add(): void {
-        this.employees.push({ _id: 1, name: "Parik" });
-    }
+    
 }
